c41111ed857c544a54143d2c12dd8c144c595847
Rohit Sharma <105491326+Rohitgut@users.noreply.github.com>
2023-08-25 15:12:56 +0530

    Update choice.js

--- choice.js
+++ choice.js
@@ -1,29 +1,6 @@
 
 var click=new Audio("music/click.mp3");
 
-var x=new Audio("music/music.mp3");
-        var button=new Audio("music/click.mp3");
-        x.autoplay=false;
-        document.getElementById('dba').addEventListener('click',function(){
-
-            
-            
-            x.loop=true;
-            x.volume=0.3;
-            
-            if(x.autoplay){
-                x.pause();
-                document.getElementById('dba').innerHTML='<i class="fa-solid fa-volume-xmark" style="color: #000000;"></i>';
-                x.autoplay=false;
-
-            }else{
-                x.play();
-                document.getElementById('dba').innerHTML='<i class="fa-solid fa-volume-high" style="color: #000000;"></i>';
-                x.autoplay=true;
-            }
-            
-        });
-
 document.getElementById('pvp').addEventListener('click',function(){
       			
       			click.play();
@@ -31,7 +8,7 @@ document.getElementById('pvp').addEventListener('click',function(){
       	    fetch("matchcard.html")
                 .then(response => response.text())
                 .then(data => {
-                    document.body.innerHTML = data;
+                    document.getElementById('contents').innerHTML = data;
 
                    
                     var script = document.createElement("script");
@@ -54,7 +31,7 @@ document.getElementById('device').addEventListener('click',function(){
       	    fetch("matchcard2.html")
                 .then(response => response.text())
                 .then(data => {
-                    document.body.innerHTML = data;
+                    document.getElementById('contents').innerHTML = data;
 
                    
                     var script = document.createElement("script");
