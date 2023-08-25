2a1c2cbd5905d2b27426bfd3a1e1ef34f304651b
Rohit Sharma <105491326+Rohitgut@users.noreply.github.com>
2023-08-25 14:42:27 +0530

    Update choice.js

--- choice.js
+++ choice.js
@@ -1,6 +1,29 @@
 
 var click=new Audio("music/click.mp3");
 
+var x=new Audio("music/music.mp3");
+        var button=new Audio("music/click.mp3");
+        x.autoplay=false;
+        document.getElementById('dba').addEventListener('click',function(){
+
+            
+            
+            x.loop=true;
+            x.volume=0.3;
+            
+            if(x.autoplay){
+                x.pause();
+                document.getElementById('dba').innerHTML='<i class="fa-solid fa-volume-xmark" style="color: #000000;"></i>';
+                x.autoplay=false;
+
+            }else{
+                x.play();
+                document.getElementById('dba').innerHTML='<i class="fa-solid fa-volume-high" style="color: #000000;"></i>';
+                x.autoplay=true;
+            }
+            
+        });
+
 document.getElementById('pvp').addEventListener('click',function(){
       			
       			click.play();
