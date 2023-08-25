f2ef7917a0e6e96bfa6350376608392bbfd4b6e5
Rohit Sharma <105491326+Rohitgut@users.noreply.github.com>
2023-08-25 14:24:38 +0530

    Add files via upload

---
+++ choice.js
@@ -0,0 +1,45 @@
+
+var click=new Audio("music/click.mp3");
+
+document.getElementById('pvp').addEventListener('click',function(){
+      			
+      			click.play();
+
+      	    fetch("matchcard.html")
+                .then(response => response.text())
+                .then(data => {
+                    document.body.innerHTML = data;
+
+                   
+                    var script = document.createElement("script");
+                    script.src = "matchcard.js";
+                    document.head.appendChild(script);
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+   		
+      });
+
+     
+
+
+document.getElementById('device').addEventListener('click',function(){
+      			
+      			click.play();
+
+      	    fetch("matchcard2.html")
+                .then(response => response.text())
+                .then(data => {
+                    document.body.innerHTML = data;
+
+                   
+                    var script = document.createElement("script");
+                    script.src = "matchcard2.js";
+                    document.head.appendChild(script);
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+   		
+      });
